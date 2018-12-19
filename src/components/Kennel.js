b92d85658e4fa6480809e3a5f5fda170ab81697a
kellymorin <kmorin06@gmail.com>
2018-12-18 20:21:25 -0600

    Updated employee information to display using state and props, render in EmployeeList component updated to use map array method

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,15 +1,32 @@
 import React, {Component} from 'react'
 import EmployeeList from "./Employee/EmployeeList"
 import LocationList from './Location/LocationList';
+import "./Kennel.css"
 
 export default class Kennel extends Component {
+  employeesFromAPI = [
+    {id: 1, name: "Jessica Younker"},
+    {id: 2, name: "Jordan Nelson"},
+    {id: 3, name: "Zoe LeBlanc"},
+    {id: 4, name: "Blaise Roberts"}
+  ]
+
+  locationsFromAPI = [
+    {id: 1, name: "Nashville North", address: "500 Circle Way"},
+    {id: 2, name: "Nashville South", address: "10101 Binary Court"}
+  ]
+
+  state={
+    employees: this.employeesFromAPI,
+    locations: this.locationsFromAPI
+  }
+
   render() {
     return (
-      <div>
-        <h3>Student Kennels</h3>
-        <LocationList />
-        <EmployeeList />
-      </div>
+      <article className="kennel">
+        <LocationList locations={this.state.locations}/>
+        <EmployeeList employees={this.state.employees}/>
+      </article>
     );
   }
 }
