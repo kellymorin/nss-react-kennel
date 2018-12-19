0d64bfd1f9b15404feda41d8ae166caca206a03c
kellymorin <kmorin06@gmail.com>
2018-12-18 21:04:47 -0600

    Moved all data to JSON file and implemented component did mount lifecycle method to set state as the component initially renders.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,50 +6,30 @@ import EmployeeList from './Employee/EmployeeList'
 import OwnerList from './Owner/OwnerList';
 
 export default class ApplicationViews extends Component{
-  employeesFromAPI = [
-    {id: 1, name: "Jessica Younker"},
-    {id: 2, name: "Jordan Nelson"},
-    {id: 3, name: "Zoe LeBlanc"},
-    {id: 4, name: "Blaise Roberts"}
-  ]
-
-  locationsFromAPI = [
-    {id: 1, name: "Nashville North", address: "500 Circle Way"},
-    {id: 2, name: "Nashville South", address: "10101 Binary Court"}
-  ]
-
-  animalsFromAPI =[
-    {id: 1, breed: "King Charles Cavalier", name: "Maci"},
-    {id: 2, breed: "Poodle", name: "Charles"},
-    {id: 3, breed: "Yorkshire Terrier", name: "Snickers" },
-    {id: 4, breed: "Kitten", name: "Snowball"},
-    {id: 5, breed: "Fish", name: "Goldy" }
-  ]
-
-  ownersFromAPI = [
-    { id: 1, name: "Ryan Tanay" },
-    { id: 2, name: "Emma Beaton" },
-    { id: 3, name: "Dani Adkins" },
-    { id: 4, name: "Adam Oswalt" },
-    { id: 5, name: "Fletcher Bangs" },
-    { id: 6, name: "Angela Lee" }
-  ]
-
-  animalsOwnersFromAPI = [
-    {id: 1, animalId: 1, ownerId: 1},
-    {id: 2, animalId: 1, ownerId: 2},
-    {id: 3, animalId: 2, ownerId: 3},
-    {id: 4, animalId: 3, ownerId: 4},
-    {id: 5, animalId: 4, ownerId: 5},
-    {id: 6, animalId: 5, ownerId: 6}
-  ]
-
   state={
-    employees: this.employeesFromAPI,
-    locations: this.locationsFromAPI,
-    animals: this.animalsFromAPI,
-    owners: this.ownersFromAPI,
-    animalsOwners: this.animalsOwnersFromAPI
+    employees: [],
+    locations: [],
+    animals: [],
+    owners: [],
+    animalsOwners: [],
+  }
+
+  componentDidMount() {
+    const newState = {}
+
+    fetch("http://localhost:5002/animals")
+        .then(r => r.json())
+        .then(animals => newState.animals = animals)
+        .then(() =>fetch("http://localhost:5002/employees").then(r => r.json()))
+        .then(employees => newState.employees = employees)
+        .then(() => fetch("http://localhost:5002/locations")
+        .then(r => r.json()))
+        .then(locations => newState.locations = locations)
+        .then(() => fetch("http://localhost:5002/owners").then(r => r.json()))
+        .then(owners => newState.owners = owners)
+        .then(() => fetch("http://localhost:5002/animalsOwners").then(r => r.json()))
+        .then(animalsOwners => newState.animalsOwners = animalsOwners)
+        .then(() => this.setState(newState))
   }
 
   render() {
